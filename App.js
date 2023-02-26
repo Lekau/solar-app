4d3dbe70fd7ffc1619f91fbe8234075d038bcef2
lekau <lekau9222@gmail.com>
2023-02-26 23:33:20 +0200

    fetched data and displayed on a component

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch } from "@react-native-material/core";
 import { Text, View } from 'react-native';
 import ProgressCircle from '@ecancan/react-native-progress-circle'
-
+import axios from "axios";
 import { styles } from './styles/main.js';
 
 export default function App() {
   const [checked, setChecked] = useState(true);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('http://localhost:5001/getInformation/2');
+      setData(result.data);
+      console.log(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [])
+
   return (
       <View style={styles.mainView}>
           <Text style={styles.title}>You are on Solar Power</Text>
           <ProgressCircle
-            percent={30}
+            percent={data.battery_percentage}
             radius={50}
             borderWidth={8}
             color="#3399FF"
@@ -20,7 +35,7 @@ export default function App() {
             size={220}
             style={styles.progressCircle}
           >
-            <Text style={{ fontSize: 28 }}>{'30%'}</Text>
+            <Text style={{ fontSize: 28 }}>{data.battery_percentage}%</Text>
           </ProgressCircle>
           <Switch value={checked} onValueChange={() => setChecked(!checked)} style={styles.switch}/>
       </View>
